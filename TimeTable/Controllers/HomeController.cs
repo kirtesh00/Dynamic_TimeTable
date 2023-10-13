af158c75f8f6c1847ccd92b61f3ce78bcc960d5b
kirte <kirte@LAPTOP-BEGR0P3A>
2023-10-13 10:17:48 +0530

    Add project files.

---
+++ TimeTable/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeTable.Models;
+
+namespace TimeTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(InputFields inputFields)
+        {
+            
+            
+                if (inputFields.Total_hours_for_week != null && inputFields.Total_hours_for_week > 0)
+                {
+                    if (inputFields.Total_Subjects == 1)
+                    {
+                        inputFields.Sub_Working_Hours = inputFields.Total_hours_for_week;
+                    }
+                    else if (inputFields.Total_Subjects == 2)
+                    {
+                        int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                        inputFields.Sub_Working_Hours = total;
+
+                    }
+                    else if (inputFields.Total_Subjects == 3)
+                    {
+
+                        int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                        inputFields.Sub_Working_Hours = total;
+                    }
+                    else
+                    {
+
+                        int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                        inputFields.Sub_Working_Hours = total;
+                    }
+                    return RedirectToAction("GeneratSubject", inputFields);
+                }
+ 
+
+            return View();
+        }
+
+
+        public ActionResult GeneratSubject(InputFields inputFields)
+        {
+            
+
+            return View(inputFields);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TimeTable/Models/InputFields.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace TimeTable.Models
+{
+
+
+    public class InputFields
+    {
+        public int Sub_Working_Hours { get; set; } 
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "No of Working days is required")]
+        [Range(1, 7, ErrorMessage = "No of Working days must be a positive number between 1 and 7")]
+        public int No_of_Working_days { get; set; }
+
+        [Required(ErrorMessage = "No of Subjects per day is required")]
+        [Range(1, 8, ErrorMessage = "No of Subjects per day must be a positive number less than 9")]
+        public int No_of_Subjects_per_day { get; set; }
+
+        [Required(ErrorMessage = "Total Subjects is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Total Subjects must be a positive number")]
+        public int Total_Subjects { get; set; }
+
+        public int Total_hours_for_week { get; set; }
+
+    }
+
+}
